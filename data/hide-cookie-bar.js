5024cd51020f537e1647bef468dafe644a590151
e_fpischedda <francesco.pischedda@abinsula.com>
2016-05-29 14:54:27 +0200

    generalized element selection strategy

--- data/hide-cookie-bar.js
+++ data/hide-cookie-bar.js
@@ -3,35 +3,38 @@ function remove_element(element){
   element.parentNode.removeChild(element);
 }
 
-function hideCookiesPolicy(tab){
+function remove_nth_element_with_class(class_selector, index){
+  elements = document.getElementsByClassName(class_selector);
+  if(elements.length > 0){
+    remove_element(elements[index]);
+    return true;
+  }
+  return false;
+}
 
-  var possible_element_ids = ['cookie-bar', 'eu-cookie-policy', 'cccwr', 'cookieChoiceInfo'];
-  var ids_length = possible_element_ids.length;
-  var cookie_div;
-  var parent;
-  for(var i=0; i<ids_length; i++){
-    element_id = possible_element_ids[i];
-    cookie_element = document.getElementById(element_id);
-    if(cookie_element != null){
-      remove_element(cookie_element);
+function remove_first_element_by_selector(selector){
+  e = document.querySelector(selector);
+  if(e){
+    remove_element(e);
+    return true;
+  }
+  return false;
+}
+
+/*
+ * function hideCookiePolicyBySelectors
+ * @selectors: is an array of selectors, first element that match the selector
+ * is removed from the DOM
+ * */
+function hideCookiePolicyBySelectors(selectors){
+  var selectors_length = selectors.length;
+  for(var i=0; i<selectors_length; i++){
+    if(remove_first_element_by_selector(selectors[i])){
       return;
     }
   }
-  // other document query strategies
-
-  //facebook seems to keep cookie banner in a div with attribute
-  //data-testid=cookie-policy-banner
-  var e = document.querySelectorAll('[data-testid="cookie-policy-banner"]');
-  if(e.length > 0){
-    remove_element(e[0]);
-  }
-
-  // www.napolisoccer.net uses classes instead of ids
-  // the class to look for seems to be pea_cook_wrapper
-  e = document.getElementsByClassName('pea_cook_wrapper');
-  if(e.length > 0){
-    remove_element(e[0]);
-  }
 }
-
-hideCookiesPolicy();
+hideCookiePolicyBySelectors([
+'#cookie-bar', '#eu-cookie-policy', '#cccwr', '#cookieChoiceInfo',
+'[data-testid="cookie-policy-banner"]',
+'.pea_cook_wrapper', '.cc-cookies']);
